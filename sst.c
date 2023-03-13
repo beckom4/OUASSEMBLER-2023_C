613a1810c38691a8eea4bdc5353f6039ff0a7795
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-13 21:04:09 +0200

    Update sst.c

--- sst.c
+++ sst.c
@@ -10,8 +10,6 @@ static struct sst_token tokens[NUM_OF_SEPERATORS] = {
    	{':',"end of label seperator"},
    	{'(',"open brackets"},
     	{')',"close brackets"},
-	{' ',"close brackets"},
-	{'\t',"close brackets"},
 };
 
 struct sst_cpu_inst {
@@ -29,7 +27,7 @@ static struct sst_cpu_inst sst_cpu_insts[NUM_OF_CMD] = {
     {"inc",sst_tag_cpu_i_inc},
     {"jmp",sst_tag_cpu_i_jmp},
     {"jsr",sst_tag_cpu_i_jsr},
-    {"lew",sst_tag_cpu_i_lea},
+    {"lea",sst_tag_cpu_i_lea},
     {"mov",sst_tag_cpu_i_mov},
     {"not",sst_tag_cpu_i_not},
     {"prn",sst_tag_cpu_i_prn},
@@ -40,9 +38,10 @@ static struct sst_cpu_inst sst_cpu_insts[NUM_OF_CMD] = {
 };
 
 struct sst sst_get_stt_from_line(const char * line) 
-{
-	const char *first_portion, *portion;
-	int i, index_of_tok, error_flag, label_flag;
+{	
+	char *first_portion;
+	int i, index_of_tok, error_flag, label_flag, command_flag;
+	char command;
 
 	struct sst res;
 
@@ -58,33 +57,66 @@ struct sst sst_get_stt_from_line(const char * line)
 		label_flag = check_label(&res, line);
 		if(label_flag == -1)
 			error_flag == 1;
+		if(error_flag != 1)
+			command_flag = check_command(line, label_flag);
+		
+				
 	}
 	
 
-
-
-
+	/*TEMPORARY - FOR CHECKS ONLY*/
+	printf("label_flag is: %d\n", label_flag);
+	printf("command_flag is: %d\n", command_flag);
+	/*TEMPORARY - FOR CHECKS ONLY*/
+	
 
 	if(error_flag == 1)
 		res.syntax_option = sst_syntax_error;
+
    	return res;
 }
 
-int is_command(const char str[])
+int check_command(const char str[], int index)
 {
-   	int left, right, mid; 
+   	int left, right, mid,i,j,pars_begin,pars_end;
+	char cmd[MAX_LINE];
+	i = index + 1, j = 0, pars_begin = 0, pars_end = 0;
+	printf("line is: %s\n", str);
+	while(pars_end != 1)
+	{
+		if(!isspace(str[i]))	
+		{
+			pars_begin = 1;
+			cmd[j] = str[i];
+			++j;
+		}
+		else if(isspace(str[i]) && pars_begin == 1)
+			pars_end = 1;
+		++i;	
+	}
+	cmd[j] = '\0';
+	printf("cmd is: %s\n", cmd);
 	left = 0;
 	right = NUM_OF_CMD - 1;
 	/*Binary search to find the command*/
 	while(left <= right)
     	{
 		mid = (left+ right)/2;
-        	if(strcmp(str, sst_cpu_insts[mid].inst_name) > 0)
-			left = mid;
-		else if(strcmp(str, sst_cpu_insts[mid].inst_name) < 0)
-			right = mid;
+		printf("flag2\n");
+        	if(strcmp(cmd, sst_cpu_insts[mid].inst_name) > 0)
+		{
+			left = mid + 1;
+			printf("flag3\n");
+		}
+		else if(strcmp(cmd, sst_cpu_insts[mid].inst_name) < 0)
+		{
+			right = mid - 1;
+			printf("flag3\n");
+		}
 		else 
+		{	
            		return sst_cpu_insts[mid].i_tag;
+		}
    	}
     	return -1;
 }
@@ -154,10 +186,7 @@ int is_label(const char str[])
 	for(i = 0; i < len; i++)
 		if(!isalpha(str[i]))
 			return -1;
-	if(str[len] != ':')
-		return -1;
-	else
-		return 1;		
+	return 1;		
 }
 
 
@@ -165,23 +194,49 @@ int check_label(struct sst *res, const char *line)
 {
 	int i, index_of_tok;
 	char first_portion[MAX_LABEL_LENGTH + 1];
-	char *portion;
+	char *token;
+	char *final_portion = (char*)malloc(sizeof(char) * MAX_LINE);
 
-	portion = strchr(line, tokens[1].tok);
-	if(portion != NULL)
-		index_of_tok = (int)(portion - line);
-	portion = strchr(portion, tokens[1].tok);
+	token = strchr(line, tokens[1].tok);
+	if(token != NULL)
+		index_of_tok = (int)(token - line);
+	else
+		final_portion = token + 1;
+	if (final_portion != NULL)
+       	 	token = strchr(final_portion, tokens[1].tok);
+	else
+        	token = NULL;
+	printf("final portion is: %s\n", final_portion);
 	/*Checking if there are more ':' in the line, meaning if there's an error.*/
-	if(portion != NULL)
-		return -1;
-	for(i = 0; i < index_of_tok; i++)
-		first_portion[i] = line[i];
-	if(is_label(first_portion) == 1)
+	if(token != NULL || index_of_tok == 0)
 	{
-		strcpy(res->label,first_portion);
-		return 1;
+		free(final_portion);
+		return -1;
 	}
 	else
-		return 0;		
+	{
+		for(i = 0; i < index_of_tok; i++)
+			first_portion[i] = line[i];
+		first_portion[index_of_tok] = '\0';
+		if(is_label(first_portion) == 1)
+		{
+			strcpy(res->label,first_portion);
+			free(final_portion);
+			return index_of_tok;
+		}
+		else
+		{
+			free(final_portion);
+			return 0;
+		}
+	}		
+}
+
+
+int main()
+{
+	char test[] = "MAIN:	inc r1, r2";
+	sst_get_stt_from_line(test);
+	return 0;
 }
 
