06fef80529d584395601b26f0ed6887c37e86815
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-17 04:15:17 +0200

    Update sst.c

--- sst.c
+++ sst.c
@@ -1,8 +1,39 @@
 #include "sst.h"
+#include "errors_checks.h"
+
+#include <search.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Function declarations:*/
+
+/**
+ * creates a SST from a line.
+ * 
+ * @param line  the line.
+ * @return struct sst the sst.
+ */
+struct sst sst_get_stt_from_line(const char * line);
+
+int check_command(const char str[], int **index);
+
+int check_reg(const char str[]);
+
+int is_empty(const char str[]);
+
+int is_comment(const char str[]);
+
+int check_directive(const char str[], int **index);
+
+int check_label(struct sst *res, const char *line);
+
+int handle_directive(int directive_flag, char last_portion[], struct sst *res, int k);
 
 struct sst_token {
-    const char tok;
-    const char * tok_description;
+	const char tok;
+	const char * tok_description;
 };
 
 static struct sst_token tokens[NUM_OF_SEPERATORS] = {
@@ -10,6 +41,7 @@ static struct sst_token tokens[NUM_OF_SEPERATORS] = {
    	{':',"end of label seperator"},
    	{'(',"open brackets"},
     	{')',"close brackets"},
+	{'"',"close brackets"}
 };
 
 struct sst_cpu_inst {
@@ -39,9 +71,9 @@ static struct sst_cpu_inst sst_cpu_insts[NUM_OF_CMD] = {
 
 struct sst sst_get_stt_from_line(const char * line) 
 {	
+	char last_portion[MAX_LINE];
 	char *token;
-	char portion[MAX_LINE], last_portion[MAX_LINE];
-	int i, j, k, index_of_tok, error_flag, label_flag, command_flag, directive_flag;
+	int i, j, error_flag, label_flag, command_flag, directive_flag;
 	int *index;
 
 	struct sst res;
@@ -59,53 +91,64 @@ struct sst sst_get_stt_from_line(const char * line)
 	{
 		/*Checking for ':', meaning for labels:*/
 		label_flag = check_label(&res, line);
-		if(label_flag == -1)
+		if(label_flag == -2)
 			error_flag = 1;
-		*index += label_flag;
-		printf("label_flag is: %d and index is: %d\n", label_flag, *index);
-		/*Making sure that we did not find any errors.*/
+		else if(label_flag != -1)
+			*index += label_flag + 1;
 		if(error_flag != 1)
 			command_flag = check_command(line, &index);
-		/*Checking if the current portion of the line is a command*/
+		/*Checking if the current portion of the line is a command or not.*/
 		if(command_flag != -1)
 			res.syntax_option = sst_instruction;
 		/*Checking if this is a directive*/
 		else if(error_flag != 1)
-			directive_flag = is_directive(line, &index);
-		/*Checking if we found a directive*/
-		if(directive_flag != -1)
-			res.syntax_option = sst_directive;
-		printf("i is: %d\n", *index);
+			directive_flag = check_directive(line, &index);			
 		j = 0;
 		while(line[*index] != '\0')
 		{
-			printf("char is: %c\t", line[*index]);
 			last_portion[j] = line[*index];
 			j++;
 			++(*index);
 		}
-		last_portion[j] = '\0';
-		printf("last_portion is: %s\n",last_portion);
-			
-		/*Extracting the rest of the text.*/
-		for(j = 0; j < NUM_OF_TOKENS; j++)
+		last_portion[j] = '\0';		
+		/****/
+		/*A CALL FOR A FUNCTION THAT CHECKS ERROR WILL COME HERE.*/
+		if(directive_flag != -1)
 		{
-			token = strchr(last_portion,tokens[j].tok);
-			if(token != NULL)
+			if(directive_flag == 0)
 			{
-				index_of_tok = (int)(token - last_portion);
-				for(k = 0; k < index_of_tok; k++)
-					portion[k] = last_portion[k];
-				portion[k] = '\0';
-				printf("portion is: %s\n", portion);
+				token = strchr(last_portion, tokens[0].tok);
+				if(token == NULL)
+				{
+					error_flag = 1;
+					res.syntax_option = sst_syntax_error;
+					strcpy(res.syntax_error_buffer,"There are no commas after .data.");
+				}
 			}
-										
+			else if(directive_flag == 1)
+			{
+				error_flag = directive_string_errors(last_portion, &res);
+				if(error_flag == 1)
+					printf("%s\n",res.syntax_error_buffer);
+			}
+			/*Checking if the directive is .extern and the user defined a label anyway.*/
+			else if(directive_flag == 2 && label_flag != -1)
+			{
+				memset(res.label, '\0', sizeof(res.label));
+				strcpy(res.asm_directive_and_cpu_instruction.syntax_directive.dir.label_array.warning,"Warning: label defining before .extern is 					forbiden");
+			}
+					
+			if(error_flag == 0)
+			{
+				handle_directive(directive_flag, last_portion, &res, 0);
+				printf("the parameter is: %s\n", res.asm_directive_and_cpu_instruction.syntax_directive.dir.string);
+				printf("warning is: %s\n", res.asm_directive_and_cpu_instruction.syntax_directive.dir.label_array.warning);
+			}
 		}
 			
 		
 						
 	}
-	
 
 	/*TEMPORARY - FOR CHECKS ONLY*/
 	printf("label_flag is: %d\n", label_flag);
@@ -158,7 +201,7 @@ int check_command(const char str[], int **index)
 }
 
 
-int is_reg(const char str[])
+int check_reg(const char str[])
 {
 	int i;
 	const char regs[][REG_SIZE] = {"r0","r1","r2","r3","r4",
@@ -201,22 +244,23 @@ int is_comment(const char str[])
 
 }
 
-int is_directive(const char str[], int **index)
+int check_directive(const char str[], int **index)
 {
 	int i, j, parse_begin,parse_end,temp;
 	char directive[MAX_LINE];
 	const char directives_arr[][WORD_SIZE] = {".data",".string",".entry",".extern"};
 	j = 0;
+	printf("string to check is: %s\n", str);
 	temp = **index;
 	while (parse_end != 1) 
 	{
-    		if (!isspace(str[**index + 1])) 
+    		if (!isspace(str[**index])) 
 		{
        			parse_begin = 1;
-        		directive[j] = str[**index + 1];
+        		directive[j] = str[**index];
         		++j;
     		} 
-		else if (isspace(str[**index + 1]) && parse_begin == 1)
+		else if (isspace(str[**index]) && parse_begin == 1)
        			parse_end = 1;
     		++(**index);
 	}
@@ -231,68 +275,137 @@ int is_directive(const char str[], int **index)
 	return -1;
 }
 
-int is_label(const char str[])
-{
-	int i;
-	size_t len;
-	len = strlen(str);
-	for(i = 0; i < len; i++)
-		if(!isalpha(str[i]))
-			return -1;
-	return 1;		
-}
-
 
 int check_label(struct sst *res, const char *line)
 {
 	int i, index_of_tok;
 	char first_portion[MAX_LABEL_LENGTH + 1];
 	char *token;
-	char *final_portion = (char*)malloc(sizeof(char) * MAX_LINE);
-	/*Making sure that the system managed to allocate enough memory for first_portion*/
-	if(first_portion == NULL)
-	{
-		printf("The system was not able to allocate enough memory for some of your text.\n");
-		exit(1);
-	}
+
 	token = strchr(line, tokens[1].tok);
-	if(token != NULL)
-		index_of_tok = (int)(token - line);
-	else
-		final_portion = token + 1;
-	if (final_portion != NULL)
-       	 	token = strchr(final_portion, tokens[1].tok);
-	else
-        	token = NULL;
-	/*Checking if there are more ':' in the line, meaning if there's an error.*/
-	if(token != NULL || index_of_tok == 0)
-	{
-		free(final_portion);
+	if(token == NULL)
 		return -1;
-	}
 	else
 	{
+		index_of_tok = (int)(token - line);		
 		for(i = 0; i < index_of_tok; i++)
 			first_portion[i] = line[i];
 		first_portion[index_of_tok] = '\0';
-		if(is_label(first_portion) == 1)
+		printf("First portion is: %s\n ", first_portion);
+		if(label_errors(first_portion, res) == 0)
 		{
 			strcpy(res->label,first_portion);
-			free(final_portion);
 			return index_of_tok;
 		}
 		else
-		{
-			free(final_portion);
-			return 0;
-		}
+			return -2;
+		
 	}		
 }
 
+int handle_directive(int directive_flag, char last_portion[], struct sst *res, int k)
+{
+	
+	int error_flag, index_of_tok;
+	long int param;
+	char *token;
+	char parameter[MAX_LINE], sub_str[MAX_LINE];
+	error_flag = 0, index_of_tok = 0;
+	res->syntax_option = sst_directive;
+	printf("last portion is: %s\n", last_portion);
+	/*Handling the directive .data*/
+	if (directive_flag == 0)
+	{
+		res->asm_directive_and_cpu_instruction.syntax_directive.dir_tag = sst_tag_dir_data;
+		token = strchr(last_portion, tokens[0].tok);
+		if(token == NULL)
+		{
+			strcpy(parameter, last_portion);
+			error_flag = directive_data_errors(parameter, res);
+			if(error_flag == 1)
+				printf("%s\n",res->syntax_error_buffer);
+			param = strtol(parameter, NULL, DEC);
+			printf("param is: %ld\n", param);
+			res->asm_directive_and_cpu_instruction.syntax_directive.dir.data_array.data_arr[k] = param;
+			printf("the number is: %ld\n", res->asm_directive_and_cpu_instruction.syntax_directive.dir.data_array.data_arr[k]);
+			return 0;
+		}
+		else
+		{
+			index_of_tok = (int)(token - last_portion);
+			strcpy(sub_str,&last_portion[index_of_tok + 1]);
+			strncpy(parameter, last_portion, index_of_tok);
+			parameter[index_of_tok] = '\0';
+			error_flag = directive_data_errors(parameter, res);
+			if(error_flag == 1)
+				printf("%s\n",res->syntax_error_buffer);
+			printf("parameter is: %s\n", parameter);
+			param = strtol(parameter, &token, DEC);
+			printf("param is: %ld\n", param);
+			res->asm_directive_and_cpu_instruction.syntax_directive.dir.data_array.data_arr[k] = param;
+			printf("the number is: %ld\n", res->asm_directive_and_cpu_instruction.syntax_directive.dir.data_array.data_arr[k]);
+			return handle_directive(directive_flag, sub_str, res, ++k);	
+		}	
 
+	}
+	/*Handling the directive .string*/
+	else if (directive_flag == 1)
+	{
+		res->asm_directive_and_cpu_instruction.syntax_directive.dir_tag = sst_tag_dir_string;
+		token = strchr(last_portion, tokens[4].tok);
+		if(token == NULL)
+			return 0;
+		else
+		{	
+			index_of_tok = (int)(token - last_portion);
+			strcpy(sub_str,&last_portion[index_of_tok + 1]);
+			printf("sub_str is: %s\n", sub_str);
+			printf("last_portion is: %s and index is: %d\n", last_portion, index_of_tok);
+			strncpy(res->asm_directive_and_cpu_instruction.syntax_directive.dir.string, last_portion, index_of_tok);
+			res->asm_directive_and_cpu_instruction.syntax_directive.dir.string[index_of_tok] = '\0';
+			return handle_directive(directive_flag, sub_str, res, 0);	
+		}	
+
+	}
+	/*Handling the directives .entry and .extern*/
+	else if (directive_flag == 2 || directive_flag == 3)
+	{
+		error_flag = directive_entext_errors(last_portion, res);
+		if(error_flag == 0)
+		{
+			token = strtok(last_portion,"' ','\t'");
+			/***/
+			/*
+			In this section we need to itterate over the label list and compare our string to the labels.
+			If it fits one of the labels, great, otherwise - error.
+			Since we don't have a label table yet, I'm writing the base of it in a comment and will fix it later on.
+		
+			for(i = 0; i < size of table list; i++)
+			{
+				if (strcmp(table_list[i],token))
+				{
+					res->asm_directive_and_cpu_instruction.syntax_directive.dir_tag = sst_tag_dir_entry;
+					strcpy(res->asm_directive_and_cpu_instruction.syntax_directive.label,token);
+					return 0;
+				}
+			}
+			/*The string after .extern does not fit any of the labels in the label table.*/
+			/*
+			res->syntax_option = sst_syntax_error;
+			strcpy(res->syntax_error_buffer,"Missing apostrophes");
+			*/
+			/***/
+		}
+	}
+	return 0;
+}
+
+
+/*TEMPORARY - FOR TESTING ONLY*/
 int main()
 {
-	char test[] = "MAIN:	mov r1, r3";
+	char test[] = "LOOP: .string \"This is the way\"";
 	sst_get_stt_from_line(test);
 	return 0;
 }
+/*TEMPORARY - FOR TESTING ONLY*/
