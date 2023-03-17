7f9fe7d70ec607a0751d748de07b8331148e289c
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-17 23:07:41 +0200

    Update sst.c

--- sst.c
+++ sst.c
@@ -7,29 +7,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*inclusion of functions from errors_checks.c*/
+extern int directive_data_errors(char str[], struct sst *res);
+extern int directive_string_errors(char str[], struct sst *res);
+extern int directive_entext_errors(char str[], struct sst *res);
+extern int label_errors(char str[], struct sst *res);
+
+
+
 /*Function declarations:*/
+
 
 /**
  * creates a SST from a line.
  * 
- * @param line  the line.
- * @return struct sst the sst.
+ * @param line - the line to analize.
+ * @return struct sst - the sst of the line.
  */
 struct sst sst_get_stt_from_line(const char * line);
 
+/**
+ * Determines if the current portion of the line is a valid command(instruction).
+ * 
+ * @param str - the portion in the line to be checked.
+	  index - a pointer to apointer that holds the address of the location of the beginning of this portion.
+ * @return .i_tag - the tag of the command if it is a command, a number 0 - 15.
+	   -1 if this portion is not a command.
+ */
 int check_command(const char str[], int **index);
 
+/**
+ * Determines if a the current portion of the line is a valid register.
+ * 
+ * @param str - the portion in the line to be checked.
+ * @return .i: a number 0 - 7 that represents the register number.
+	   -1 if this portion is not a valid register.
+ */
 int check_reg(const char str[]);
 
+/**
+ * Determines if a the line is an empty line or not.
+ * 
+ * @param str - the line.
+ * @return 1 if the line is empty.
+	   0 if it's not.
+ */
 int is_empty(const char str[]);
 
+/**
+ * Determines if a the line is a comment or not.
+ * 
+ * @param str - the line.
+ * @return 1 if the line is a comment.
+	   0 if it's not.
+ */
 int is_comment(const char str[]);
 
+
+/**
+ * Determines if a the current portion is a valid directive.
+ * 
+ * @param str - the portion to be checked.
+	  index - a pointer to a pointer to the address of the beginning of the portion.
+ * @return i - A number 0-3 that represents the tag of the directive.
+	   -1 if it's not a valid directive.
+ */
 int check_directive(const char str[], int **index);
 
+/**
+ * Determines if a the current portion is a valid label.
+ * 
+ * @param str - the portion to be checked.
+	  struct sst - the sst of the line.
+ * @return label_flag - the size of the label, if it is a label.
+	   -1 if it's not a valid label.
+ */
 int check_label(struct sst *res, const char *line);
 
+/**
+ * Builds the sst of the directive recursively.
+ * 
+ * @param directive_flag - the tag of the directive.
+	  last_portion - the portion to be checked.
+	  struct sst - the sst of the line.
+ * @return 0
+ */
 int handle_directive(int directive_flag, char last_portion[], struct sst *res, int k);
+
+/**
+ * Builds the sst of the command recursively.
+ * 
+ * @param command_flag - the tag of the directive.
+	  last_portion - the portion to be checked.
+	  struct sst - the sst of the line.
+	  num_of_operands - the current number of operands that the function handled.
+ * @return 0
+ */
+int handle_command(int command_flag, char last_portion[], struct sst *res, int num_of_operands);
 
 struct sst_token {
 	const char tok;
