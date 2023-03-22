1d2ab180bbef989aca331862ecc97ca865d1bcdb
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-23 00:04:14 +0200

    Create assembler.h

---
+++ assembler.h
@@ -0,0 +1,17 @@
+#ifndef maman14_assembler
+#define maman14_assembler
+
+
+
+/**
+ * @brief the main assembler function(API).
+ * 
+ * @param[in] number_of_files - number of files
+ * @param[in] file_names - the files name.
+ * @return returns the number of files that were sucessfully assembled.
+ */
+int assembler_main(int number_of_files, char **file_names);
+
+
+
+#endif
