ac80a931f92b370e5733f514e69b80e556509e62
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-25 04:43:24 +0300

    Update errors_checks.h

--- errors_checks.h
+++ errors_checks.h
@@ -1,1 +1,26 @@
 #define LEN_OF_CMD_ARR 16
+#define MAX_ASCII 127
+#define REG_SIZE 2
+
+
+int label_errors(char str[], struct sst *res);
+
+int directive_data_errors(char str[], struct sst *res);
+
+int directive_string_errors(char str[], struct sst *res);
+
+int directive_entext_errors(char str[], struct sst *res);
+
+int no_operands_errors(char str[], struct sst *res);
+
+int operand_label_errors(char str[], struct sst *res);
+
+int is_empty_error(char str[]);
+
+int is_directive(char str[]);
+
+int is_reg(char str[]);
+
+int is_command(char str[]);
+
+int is_ascii(char c);
