ac632e52a722e6789d07c91b88a36e27189d8e5c
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-26 20:26:05 +0300

    Update preprocessor.h

--- preprocessor.h
+++ preprocessor.h
@@ -1,14 +1,6 @@
-/* -------------------------------------------- */
+/*This file serves as the preprocessor - It consists of the functions that find macros in the files, check them for errors, analyze them and save their content.*/
 
-/** 
-* MAMAN 14, FILE: preprocessor.h
-* @authors: Linoy daniel & Omri Beck
-* This module is the preprocessor - It consists of the functions that find macros in the files, check them for errors, analyze them and save their content.
-*/
-
-/* -------------------------------------------- */
-
-
+#define FOUND_ERROR -2
 #define END_PROGRAM 0
 #define MAX_LINE 80
 #define MAX_CMD_ARR 16
@@ -16,7 +8,9 @@
 #define MCR_BODY 2
 #define ENDMCR 3
 #define DONE 4
+#define END_FUNCTION -1
 #define MCR_SIZE 3
 #define ENDMCR_SIZE 6
 #define MAX_FILE_NAME 100
-#define FOUND_ERROR -2
+
+int preprocessor(char *file_name);
