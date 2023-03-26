ccf9ec349443c1821c295ddd4eadc3a1a5b88f9a
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-26 17:20:50 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -42,7 +42,22 @@ struct asm_label {
         asm_label_type_code,
         asm_label_type_data,
         asm_label_type_data_entry,
-        asm_label_type_code_entry
+        asm_label_type_code_entrysst_fianl_test: sst.o errors_checks.o handle_parse.o preprocessor.o dynarray.o bmc.o assembler.o
+	gcc -ansi -Wall -pedantic sst.o errors_checks.o handle_parse.o preprocessor.o dynarray.o bmc.o assembler.o -o sst_final_test
+sst.o: sst.c sst.h errors_checks.h handle_parse.h
+	gcc -c -ansi -Wall -pedantic sst.c -o sst.o
+errors_checks.o: errors_checks.c errors_checks.h sst.h handle_parse.h
+	gcc -c -ansi -Wall -pedantic errors_checks.c -o errors_checks.o
+handle_parse.o: handle_parse.c errors_checks.h sst.h
+	gcc -c -ansi -Wall -pedantic handle_parse.c -o handle_parse.o
+preprocessor.o: preprocessor.c preprocessor.h
+	gcc -c -ansi -Wall -pedantic preprocessor.c -o preprocessor.o
+dynarray.o: dynarray.c dynarray.h
+	gcc -c -ansi -Wall -pedantic dynarray.c -o dynarray.o
+bmc.o: bmc.c bmc.h dynarray.h
+	gcc -c -ansi -Wall -pedantic bmc.c -o bmc.o
+assembler.o: assembler.c dynarray.h bmc.h sst.h
+	gcc -c -ansi -Wall -pedantic assembler.c -o assembler.o
     } asm_label_type;
 
     int line_of_definition;
