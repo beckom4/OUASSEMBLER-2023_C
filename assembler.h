c1ea8b9057d523f7d0c2c22c165bb0dfa99daae1
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-03-26 17:10:45 +0300

    Update assembler.h

--- assembler.h
+++ assembler.h
@@ -1,13 +1,24 @@
-#ifndef maman14_assembler
-#define maman14_assembler
+/* -------------------------------------------- */
+
+/** 
+* MAMAN 14, FILE: assembler.h
+* @authors: Linoy daniel & Omri Beck
+* This module reads files given to us by the user and converts each line in their content into regular binary code.
+* In addition, before cooding the text, the assembler goes over the text, picks up label definitions and inserts them into the label table.
+**/
+
+/* -------------------------------------------- */
 
 
+#ifndef ASSEMBLER
+#define ASSEMBLER
 
+#define MAX_LINE_LEN 80
 /**
  * @brief the main assembler function(API).
  * 
- * @param[in] number_of_files - number of files
- * @param[in] file_names - the files name.
+ * @param number_of_files - number of files
+ * @param file_names - the files name.
  * @return returns the number of files that were sucessfully assembled.
  */
 int assembler_main(int number_of_files, char **file_names);
