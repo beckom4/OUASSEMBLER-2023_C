90e2090cd6b4e8f6d84d54350fab1d38d41d1341
beckom4 <126347234+beckom4@users.noreply.github.com>
2023-04-10 18:21:13 +0300

    Create preprocessor.h

---
+++ preprocessor/preprocessor.h
@@ -0,0 +1,17 @@
+/* -------------------------------------------- */
+
+/** 
+* MAMAN 14, FILE: preprocessor.h
+* @authors: Linoy daniel & Omri Beck
+* This module is the preprocessor - It consists of the functions that find macros in the files, check them for errors, analyze them and save their content.
+*/
+
+/* -------------------------------------------- */
+
+
+
+
+
+/*Function declarations:*/
+
+int preprocessor( char *file_name);
